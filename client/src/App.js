68ffb44a0edd998f281a70af1a226a1790137c06
MumuG <79053287+GianniGH@users.noreply.github.com>
2023-07-03 18:46:35 -0400

    SignIn Component / View
    Creacion vista SignIn y ajustes css

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Views/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import SignIn from './Components/SignIn'
 import UserView from './Components/UserView';
 import AddClass from './Components/AddClass';
 import Alumnos from './Components/Alumnos';
@@ -12,6 +11,7 @@ import ViewStudent from './Views/ViewStudent';
 import Edit from './Views/Edit';
 import { NavbarMUI } from './Components/NavbarMUI';
 import { SignUp } from './Views/SignUp';
+import { SignIn } from './Views/SignIn';
 
 
 
@@ -24,7 +24,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/sign-in'element={<SignIn />}/>
+      <Route path="/sign-in" element={<SignIn />} />
       <Route path='/user-view' element={<UserView />}/>
       <Route path='/add-class' element={<AddClass />}/>
       <Route path='/alumnos' element={<Alumnos />}/>
---
+++ client/src/Views/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignInForm } from '../Components/SignInForm';
+import '../Styles/SignUpForm.css';
+import '../Styles/Home.css';
+
+export const SignIn = () => {
+  return (
+    <div className='signup-container'>
+        <SignInForm />
+    </div>
+  )
+}
